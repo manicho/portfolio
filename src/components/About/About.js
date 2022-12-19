36810aea179e3ac1f1cccc8d31916340af9edc55
manicho <manicho@gmail.com>
2022-12-18 22:13:51 -0300

    feat:added list link with independent opening, work in progress

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import GitHubIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { useState } from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 // import Home from './Home'
 import { about } from './content'
 import './About.scss'
@@ -8,35 +9,65 @@ import './About.scss'
 const About = () => {
   const { name, role, description, resume, social } = about
 
+  const [showText, setShowText] = useState('')
+
+  const toggleText = (e) => {
+    e.preventDefault()
+    // setShowText
+    console.log(e.target)
+    if (e.type === 'mouseenter') setShowText(e.target.id)
+    else if (e.type === 'mouseleave') setShowText('')
+  }
+
   return (
     <main className='about-container'>
-      About
-      {/* {role && <h2>A {role}.</h2>} */}
-      {/* <h2>{description}</h2> */}
+      <p>About</p>
 
-      {/* <div>
+      <div className='link-list'>
         {resume && (
-          <a href={resume}>
-            <span>Resume</span>
+          <a href={resume} id='resume' className='link' onMouseEnter={toggleText}
+          onMouseLeave={toggleText}>
+            R
+            <p className='link-text' hidden={showText !== 'resume'}>resume</p>
           </a>
         )}
 
         {social && (
           <>
             {social.github && (
-              <a href={social.github} arial-label='github'>
+              <a
+                href={social.github}
+                arial-label='github'
+                id='github'
+                className='link'
+                onMouseEnter={toggleText}
+                onMouseLeave={toggleText}
+              >
                 <FaGithub className='social-icon' />
+                <p className='link-text' hidden={showText !== 'github'}>
+                  {social.github}
+                </p>
               </a>
             )}
 
             {social.linkedin && (
-              <a href={social.linkedin} arial-label='linkedin'>
+              <a
+                href={social.linkedin}
+                arial-label='linkedin'
+                id='linkedin'
+                className='link'
+                onMouseEnter={toggleText}
+                onMouseLeave={toggleText}
+              >
                 <FaLinkedin className='social-icon' />
+                <p className='link-text' hidden={showText !== 'linkedin'}>
+                  {social.linkedin}
+                </p>
               </a>
             )}
           </>
         )}
-      </div> */}
+      </div>
     </main>
   )
 }
